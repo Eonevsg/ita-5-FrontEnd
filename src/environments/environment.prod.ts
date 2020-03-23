050db01b3056a04ef9130cda611eeb29a4b76778
gaan3654 <g.anuzyte@yahoo.com>
2020-03-23 17:50:49 +0200

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: "https://ita-5-back-staging.herokuapp.com",
-  answersUrl: "http://ita-5-back-staging.herokuapp.com/api/answer",
-  apiUrl: "http://ita-5-back-staging.herokuapp.com/api"
+  answersUrl: "https://ita-5-back-staging.herokuapp.com/api/answer",
+  apiUrl: "https://ita-5-back-staging.herokuapp.com/api"
 };
