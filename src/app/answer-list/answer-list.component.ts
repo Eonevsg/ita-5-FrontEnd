c7a8d2e8006c9988fcd40456619176f57cfdec00
[Justas] <[justaslescinskas@gmail.com]>
2020-03-24 14:39:12 +0200

    Implemented log out button

--- src/app/answer-list/answer-list.component.ts
+++ src/app/answer-list/answer-list.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit } from "@angular/core";
-import { AnswerViewModel } from "../shared/answerViewModel";
-import { FormService } from "../form-service/form.service";
+import {Component, OnInit} from '@angular/core';
+import {AnswerViewModel} from '../shared/answerViewModel';
+import {FormService} from '../form-service/form.service';
+import {AuthService} from '../services/auth/auth.service';
+import {Router} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
-  selector: "app-answer-list",
-  templateUrl: "./answer-list.component.html",
-  styleUrls: ["./answer-list.component.css"],
+  selector: 'app-answer-list',
+  templateUrl: './answer-list.component.html',
+  styleUrls: ['./answer-list.component.css'],
   providers: [FormService]
 })
 export class AnswerListComponent implements OnInit {
   answers: AnswerViewModel[];
 
-  constructor(private formService: FormService) {}
+  constructor(
+    private formService: FormService,
+    private authService: AuthService,
+    private router: Router,
+    private location: Location
+  ) {
+  }
 
   ngOnInit(): void {
     this.formService.findAllAnswers().subscribe(data => {
       this.answers = data;
     });
+  }
+
+  logOutButtonClick() {
+    this.authService.logOut();
+    this.location.replaceState('/'); // clears browser history so they can't navigate with back button
+    this.router.navigate(['home']);
   }
 }
