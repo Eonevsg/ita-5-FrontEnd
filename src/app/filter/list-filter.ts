980a808402dd3acb01b7b678fe3d0c8a6a2b0e54
Eone <ivan.gorbun@stud.vgtu.lt>
2020-03-24 17:52:45 +0200

    Filter is done

--- src/app/filter/list-filter.ts
+++ src/app/filter/list-filter.ts
@@ -4,12 +4,17 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "search"
 })
 export class SearchPipe implements PipeTransform {
-  public transform(values: any[], keys: string, term: string) {
-      console.log(keys);
-      console.log(term);
-      if(!values || !term){
-          return values;
-      }
-    return values.filter(item => item.person.name.indexOf(term) !== -1);
+  public transform(values: any[], filters: any) {
+    if (!values || !filters) {
+      return values;
+    }
+
+    return values.filter(item => {
+      return (
+        item.person.name.toLowerCase().indexOf(filters.name.toLowerCase()) >= 0 && 
+        item.person.surname.toLowerCase().indexOf(filters.surname.toLowerCase()) >= 0 &&
+        item.person.uni.toLowerCase().indexOf(filters.uni.toLowerCase()) >= 0
+      );
+    });
   }
 }
