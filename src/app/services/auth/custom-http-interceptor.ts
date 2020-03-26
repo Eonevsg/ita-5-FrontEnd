652d0e912731b223cbeca928b539e1a8e4f07666
Vyturys <vytur.jok@gmail.com>
2020-03-26 12:53:34 +0200

    Redirects to login page when server returns 403 forbidden

--- src/app/services/auth/custom-http-interceptor.ts
+++ src/app/services/auth/custom-http-interceptor.ts
@@ -1,18 +1,20 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {AuthService} from './auth.service';
 import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class CustomHttpInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return this.handleAccess(req, next);
   }
 
-  private  handleAccess(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  private handleAccess(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.authService.getToken();
     let changedRequest = request;
 
@@ -29,7 +31,17 @@ export class CustomHttpInterceptor implements HttpInterceptor {
     changedRequest = request.clone({
       headers: newHeader
     });
-    return next.handle(changedRequest);
+    return next.handle(changedRequest).pipe(tap(() => {
+      },
+      (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status !== 403) {
+            return;
+          }
+          this.router.navigate(['login']);
+        }
+      }
+    ));
   }
 
 }
