60e0be6fe665781a801aace98c8a16204daf88eb
Eone <ivan.gorbun@stud.vgtu.lt>
2020-03-27 17:49:05 +0200

    Data disappears from details form

--- src/app/answer-details/answer-details.component.ts
+++ src/app/answer-details/answer-details.component.ts
@@ -1,22 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {Answer} from '../models/answer';
-import {AnswerViewModel} from '../shared/answerViewModel';
-import {FormService} from '../services/form-service/form.service';
-import {ActivatedRoute} from '@angular/router';
-import {from, Observable} from 'rxjs';
-import {switchMap} from 'rxjs/operators';
-import {FormBuilder, Validators} from '@angular/forms';
-import {Person} from '../models/person';
+import { Component, OnInit } from "@angular/core";
+import { Answer } from "../models/answer";
+import { AnswerViewModel } from "../shared/answerViewModel";
+import { FormService } from "../services/form-service/form.service";
+import { ActivatedRoute } from "@angular/router";
+import { from, Observable } from "rxjs";
+import { switchMap } from "rxjs/operators";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Person } from "../models/person";
 
 @Component({
-  selector: 'app-answer-details',
-  templateUrl: './answer-details.component.html',
-  styleUrls: ['./answer-details.component.css']
+  selector: "app-answer-details",
+  templateUrl: "./answer-details.component.html",
+  styleUrls: ["./answer-details.component.css"]
 })
 export class AnswerDetailsComponent implements OnInit {
   public answer$: Observable<AnswerViewModel>;
   public questions: Answer[];
-  public radioQuestionID: string[] = ['1', '2'];
+  public radioQuestionID: string[] = ["1", "2"];
   public showModal: boolean;
   public person$: Observable<Person>;
 
@@ -32,30 +32,29 @@ export class AnswerDetailsComponent implements OnInit {
     private formService: FormService,
     private route: ActivatedRoute,
     private fb: FormBuilder
-  ) {
-  }
+  ) {}
 
   valuationForm = this.fb.group({
     applicationValuation: [
-      '',
+      "",
       [
         Validators.min(0),
         Validators.max(100),
-        Validators.pattern('^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ0-9 \\.,\\-\'"]+$')
+        Validators.pattern("^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ0-9 \\.,\\-'\"]+$")
       ]
     ],
     interviewValuation: [
-      '',
+      "",
       [
         Validators.min(0),
         Validators.max(100),
-        Validators.pattern('^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ0-9 \\.,\\-\'"]+$')
+        Validators.pattern("^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ0-9 \\.,\\-'\"]+$")
       ]
     ],
-    notes: ['', [Validators.maxLength(450)]]
+    notes: ["", [Validators.maxLength(450)]]
   });
   acceptanceForm = this.fb.group({
-    state: ['', []]
+    state: ["", []]
   });
 
   ngOnInit(): void {
@@ -65,10 +64,12 @@ export class AnswerDetailsComponent implements OnInit {
 
     this.answer$ = from(this.route.paramMap).pipe(
       switchMap(params => {
-        return this.formService.fetchAnswer({id: params.get('id')});
+        return this.formService.fetchAnswer({ id: params.get("id") });
       })
     );
     this.answer$.subscribe(data => {
+      console.log(data.person.extra);
+      this.setExistingExtraValue(data.person.extra);
       this.updateStatus(data.person);
       this.personId = data.person.id;
       this.email = data.person.email;
@@ -87,7 +88,8 @@ export class AnswerDetailsComponent implements OnInit {
     if (this.notes.value) {
       this.tempNotes = this.notes.value;
     }
-
+console.log("tempappval", this.tempApplVal);
+console.log("appval", )
     this.formService.patchPerson({
       id: this.personId,
       extra: {
@@ -102,23 +104,23 @@ export class AnswerDetailsComponent implements OnInit {
     this.acceptanceForm.markAllAsTouched();
     this.statusValue = null;
     console.log(this.state.value);
-    if (this.state.value === 'yes') {
-      this.statusValue = 'Priimta';
-    } else if (this.state.value === 'no') {
-      this.statusValue = 'Atmesta';
+    if (this.state.value === "yes") {
+      this.statusValue = "Priimta";
+    } else if (this.state.value === "no") {
+      this.statusValue = "Atmesta";
     }
     window.location.href = this.getEmailOpenString(this.email);
     this.formService.patchPerson({
       id: this.personId,
-      extra: {status: this.statusValue}
+      extra: { status: this.statusValue }
     });
   }
 
   updateStatus(person: Person): any {
-    if (person.extra.status.toLowerCase() === 'nauja') {
+    if (person.extra.status.toLowerCase() === "nauja") {
       return this.formService.patchPerson({
         id: person.id,
-        extra: {status: 'Perskaityta'}
+        extra: { status: "Perskaityta" }
       });
     }
   }
@@ -130,9 +132,9 @@ export class AnswerDetailsComponent implements OnInit {
   getAnswer(str: string, qId: string) {
     if (this.radioQuestionID.includes(qId)) {
       if (str) {
-        return 'Ne. ' + str;
+        return "Ne. " + str;
       } else {
-        return 'Taip';
+        return "Taip";
       }
     } else {
       return str;
@@ -140,19 +142,19 @@ export class AnswerDetailsComponent implements OnInit {
   }
 
   get applicationValuation() {
-    return this.valuationForm.get('applicationValuation');
+    return this.valuationForm.get("applicationValuation");
   }
 
   get interviewValuation() {
-    return this.valuationForm.get('interviewValuation');
+    return this.valuationForm.get("interviewValuation");
   }
 
   get notes() {
-    return this.valuationForm.get('notes');
+    return this.valuationForm.get("notes");
   }
 
   get state() {
-    return this.acceptanceForm.get('state');
+    return this.acceptanceForm.get("state");
   }
 
   show() {
@@ -165,5 +167,20 @@ export class AnswerDetailsComponent implements OnInit {
 
   getEmailOpenString(email: string) {
     return `mailto:${email}?subject=IT Akademija`;
+  }
+  setExistingExtraValue(extra: any) {
+    if (extra.applicationValuation) {
+      this.valuationForm.controls["applicationValuation"].setValue(
+        extra.applicationValuation
+      );
+    }
+    if (extra.interviewValuation) {
+      this.valuationForm.controls["interviewValuation"].setValue(
+        extra.interviewValuation
+      );
+    }
+    if (extra.notes) {
+      this.valuationForm.controls["notes"].setValue(extra.notes);
+    }
   }
 }
