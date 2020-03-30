68f25b5a962e836e165a5f82340c039aeabfb6b4
Eone <ivan.gorbun@stud.vgtu.lt>
2020-03-30 16:45:43 +0300

    custom interceptor loop replaced by forEach loop

--- src/app/services/auth/custom-http-interceptor.ts
+++ src/app/services/auth/custom-http-interceptor.ts
@@ -1,47 +1,61 @@
-import {Injectable} from '@angular/core';
-import {HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {AuthService} from './auth.service';
-import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
-import {Router} from '@angular/router';
+import { Injectable } from "@angular/core";
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpRequest
+} from "@angular/common/http";
+import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class CustomHttpInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService, private router: Router) {
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     return this.handleAccess(req, next);
   }
 
-  private handleAccess(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  private handleAccess(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     const token = this.authService.getToken();
     let changedRequest = request;
 
-    const headerSettings: { [name: string]: string | string[]; } = {};
+    const headerSettings: { [name: string]: string | string[] } = {};
 
-    for (const key of request.headers.keys()) {
-      headerSettings[key] = request.headers.getAll(key);
-    }
+    request.headers
+      .keys()
+      .forEach(key => (headerSettings[key] = request.headers.getAll(key)));
+
     if (token) {
       headerSettings.Authorization = token;
     }
-    headerSettings['Content-Type'] = 'application/json';
+    headerSettings["Content-Type"] = "application/json";
     const newHeader = new HttpHeaders(headerSettings);
     changedRequest = request.clone({
       headers: newHeader
     });
-    return next.handle(changedRequest).pipe(tap(() => {
-      },
-      (err: any) => {
-        if (err instanceof HttpErrorResponse) {
-          if (err.status !== 403) {
-            return;
+    return next.handle(changedRequest).pipe(
+      tap(
+        () => {},
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status !== 403) {
+              return;
+            }
+            this.router.navigate([""]);
           }
-          this.router.navigate(['']);
         }
-      }
-    ));
+      )
+    );
   }
-
 }
