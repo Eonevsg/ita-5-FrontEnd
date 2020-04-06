d1913e943fa75a6640572105649ccf1311a20c68
Eone <ivan.gorbun@stud.vgtu.lt>
2020-04-06 17:27:43 +0300

    Something went wrong with floatign window

--- src/app/answer-details/answer-details.component.ts
+++ src/app/answer-details/answer-details.component.ts
@@ -111,7 +111,7 @@ export class AnswerDetailsComponent implements OnInit {
   }
 
   show() {
-    document.getElementById("overlay").classList.add("fadeIn");
+    // document.getElementById("overlay").classList.add("fadeIn");
     this.showModal = true;
   }
 
