7f5640ade1075c71ed4e1a96b0e955e9323155b3
Eone <ivan.gorbun@stud.vgtu.lt>
2020-04-07 17:22:11 +0300

    list page desktop design

--- src/app/answer-list/answer-list.component.ts
+++ src/app/answer-list/answer-list.component.ts
@@ -40,6 +40,10 @@ export class AnswerListComponent implements OnInit {
   constructor(private formService: ApplicationFormService) {}
 
   ngOnInit(): void {
+    document.getElementById("header").classList.add("width1200");
+    document.getElementById("footer").classList.add("width1200");
+    document.getElementById("main2").classList.add("width1200");
+
     this.formService.findAllAnswers().subscribe((data) => {
       this.answers = data;
       console.log(data);
@@ -47,6 +51,11 @@ export class AnswerListComponent implements OnInit {
       console.log(this.persons);
       this.sortedPersons = this.persons.slice();
     });
+  }
+  ngOnDestroy(){
+    document.getElementById("header").classList.remove("width1200");
+    document.getElementById("footer").classList.remove("width1200");
+    document.getElementById("main2").classList.add("width1200");
   }
   updateFilters(): void {
     this.filters = Object.assign({}, this.fields);
